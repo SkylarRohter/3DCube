aa0c35bbcf41aa840c2247c6036d25f6775af483
skylarrohter <skyriversub@gmail.com>
2025-01-24 08:39:59 -0800

    rotation angles can be inaccurately calculated in 2d with o(n^2) time. need to optimize at some point

--- 2d/Render2D.py
+++ 2d/Render2D.py
@@ -1,12 +1,37 @@
+import math
+from os.path import exists
+
 import numpy as np
 
+
+origin = (20,40)
+
+a = 3.141592653589793
+# a = int(np.pi/2)
+
+def calc_x(x,y,t):
+    return int((x * (np.cos(t))) - (y * (np.sin(t))))
+def calc_y(x,y,t):
+    return int((x * (np.sin(t))) + (y * (np.cos(t))))
+
+
+def calc_slope(co1, co2):
+    return [(co2[1] - co1[1]),(co2[0] - co1[0])]
 def render():
     rows = 90
     cols = 45
     content = [["."] * cols for _ in range(rows)]
 
     # (x,y, char)
-    grid = [(20,40, "O"), (30,40, "O"), (30,50,"O"),]
+    grid = [(20,40, "*"), (calc_x(30,40, a), calc_y(30,40, a), "@"), (calc_x(30,50, a), calc_y(30,50, a),"O"),]
+    slopes = []
+    for i in range(len(grid)):
+        for k in range(len(grid)):
+            if not k <= i :
+                slopes.append((grid[i],grid[k],calc_slope(grid[i], grid[k])))
+
+    print(slopes)
+
     for (y, x, c) in grid: content[x][y] = c
     width = len(str(max(rows, cols) - 1))
     content_line = "# | values |"
